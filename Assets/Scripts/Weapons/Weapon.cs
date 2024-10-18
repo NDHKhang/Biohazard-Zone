b242e95abc364e3f5c7fce4522a7201c2c4f59c8
Khang Nguyen <dhoang.khang1504@gmail.com>
2024-10-19 02:19:15 +0700

    +Weapon Fire & VFX

---
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] private Camera FPCamera;
+
+    [SerializeField] WeaponData weaponData;
+
+    [Tooltip("Determine how far the bullet go")]
+    [SerializeField] private float range = 50f;
+
+    [Header("References")]
+    [SerializeField] private ParticleSystem muzzleFlash;
+    [SerializeField] private GameObject hitEffect;
+    [SerializeField] private GameObject parent;
+
+    private float weaponDamage;
+    void Start()
+    {
+        weaponDamage = weaponData.damage;
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnFire(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            Fire();
+        }
+    }
+
+    void Fire()
+    {
+        muzzleFlash.Play();
+
+        RaycastHit hit; //Variable for storing the information of WHAT WE HIT
+
+        //Firing
+        if(Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
+        {
+            Debug.Log("Hit" + hit.transform.name);
+
+            Enemy enemy = hit.transform.GetComponent<Enemy>();
+            if (enemy != null)
+            {
+                enemy.TakeDamage(weaponDamage);
+            }
+
+            GameObject impact = Instantiate(hitEffect, hit.point, Quaternion.LookRotation(hit.normal), parent.transform);
+            Destroy(impact, 1f);
+        }
+    }
+}
---
+++ Assets/Scripts/Weapons/WeaponData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Weapon", menuName = "GameConfiguration/Weapon")]
+public class WeaponData : ScriptableObject
+{
+    public float damage;
+}
